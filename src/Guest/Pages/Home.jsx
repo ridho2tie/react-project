d1852fc1742d4c832e242a513a62fc8b93b40f58
ridho2tie <fayiz23ti@mahasiswa.pcr.ac.id>
2025-06-15 21:28:13 +0700

    merubah import banner

--- src/Guest/Pages/Home.jsx
+++ src/Guest/Pages/Home.jsx
@@ -2,7 +2,7 @@ import HeroSection from '../Components/HeroSection';
 import TrendingProducts from '../Components/TrendingProducts';
 import BestSelling from '../Components/BestSelling';
 import Testimonial from '../Components/Testimonial';
-import Banner from '../Components/banner';
+import Banner from '../Components/Banner';
 import BrandPartners from '../Components/BrandPartners';
 
 export default function Home() {
