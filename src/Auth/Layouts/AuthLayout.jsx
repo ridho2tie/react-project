684da5f333a4a5c98f369da27ef2ad21e6941100
ridho2tie <fayiz23ti@mahasiswa.pcr.ac.id>
2025-06-19 20:50:41 +0700

    perbaiki authlayouts 2

--- src/Auth/Layouts/AuthLayout.jsx
+++ src/Auth/Layouts/AuthLayout.jsx
@@ -1,9 +1,9 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../../Guest/Components/Navbar";
+
 
 export default function AuthLayout() {
   return (
-      <div className="h-218 flex items-center inset-0 justify-center bg-gradient-to-r from-gray-600 to-white">
+      <div className="h-250 flex items-center inset-0 justify-center bg-gradient-to-r from-gray-600 to-white">
         <div className="w-full max-w-7xl bg-white rounded-lg shadow-lg flex overflow-hidden">
           <Outlet />
         </div>
