517721e90501f419005256cb3cf23da982ecc1de
ridho2tie <fayiz23ti@mahasiswa.pcr.ac.id>
2025-07-07 00:41:02 +0700

    fix service 3

--- src/Services/FAQAPI.js
+++ src/Services/FAQAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = "https://jboaqwdcfkvttqnkaabh.supabase.co/rest/v1/FAQ"
+const API_URL = "https://jboaqwdcfkvttqnkaabh.supabase.co"
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impib2Fxd2RjZmt2dHRxbmthYWJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk2NDk0NjIsImV4cCI6MjA2NTIyNTQ2Mn0.MXJLKqTVfq_jocKFPum6sNKSuxIL7n9WYKm3XdWIoOE"
 
 const headers = {
